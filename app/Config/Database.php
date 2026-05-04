3e004a5e9bd0c746cc07155665c40135945eb50e
Danis Keysara Saputra <daniskeysara@gmail.com>
2026-05-05 00:02:28 +0700

    Enable SSL for Azure Database

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -66,7 +66,7 @@ class Database extends Config
         'charset'     => 'utf8',
         'DBCollat'    => 'utf8_general_ci',
         'swapPre'     => '',
-        'encrypt'     => false,
+        'encrypt'     => true,
         'compress'    => false,
         'strictOn'    => false,
         'failover'    => [],
